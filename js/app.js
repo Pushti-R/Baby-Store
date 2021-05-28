c2212307bcfc56d93962411d1d9d52f2e06cb440
Pushti <pushti.ratanghayra@gmail.com>
2021-05-28 15:09:12 +0530

    Completed

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.wrap').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".pbtn",
+        nextArrow:".nbtn",
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
+    })
+
+    $('.trigger').click(function(){
+        $('.contwrap').toggleClass('scale')
+    })
+})
